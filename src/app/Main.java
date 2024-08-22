6905bb53dd5dcd32d93714de947433b5a80e7f71
Kyrylo Kusovsky <kkirilll2006@gmail.com>
2024-08-22 13:12:35 +0300

    Add addition

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,8 +1,19 @@
 package app;
 
 public class Main {
+    static int a;
+    static int b;
+    static int res;
     public static void main(String[] args) {
         System.out.println("My new app");
         System.out.println("For calculating");
+
+        a = 10;
+        b = 13;
+        res = add(a, b);
+        System.out.println("Result:" + res);
+    }
+    private static int add(int a, int b){
+        return a+b;
     }
 }
